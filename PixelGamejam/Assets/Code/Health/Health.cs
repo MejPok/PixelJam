da0579ea4426572c09f1528c56f7d06b2df1996b
lukasprogram <lukas.je@post.cz>
2025-05-12 21:15:44 +0200

    GainBones

--- PixelGamejam/Assets/Code/Health/Health.cs
+++ PixelGamejam/Assets/Code/Health/Health.cs
@@ -15,6 +15,14 @@ public class Health : MonoBehaviour
             Dead();
         }
     }
+    public void GainBones(int amount){
+        if(BonesLeft + amount >= BonesMax){
+            BonesLeft = BonesMax;
+        } else {
+            BonesLeft += amount;
+        }
+
+    }
 
     void Dead(){
         Debug.Log("Deaht not implemented");
