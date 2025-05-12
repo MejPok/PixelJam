7b2e08d268e10d992b41a0a32ac84c8331ba8686
lukasprogram <lukas.je@post.cz>
2025-05-12 20:37:38 +0200

    AllStatesDone

--- PixelGamejam/Assets/Code/Movement/MovementStates/JustArmsScriptable.cs
+++ PixelGamejam/Assets/Code/Movement/MovementStates/JustArmsScriptable.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="FullState")]
-public class FullBodyScriptable : ScriptableMovementState
+[CreateAssetMenu(fileName ="ArmsState")]
+public class JustArmsScriptable : ScriptableMovementState
 {
-    public override void Jump(){
-        rb.velocity = new Vector2(rb.velocity.x, JumpForce * JumpMultiplier);
-    }
-
     public override void Move(float speed)
     {
         float horizontalInput = Input.GetAxis("Horizontal"); //Important!
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y); 
+    }
+
+    public override void Jump(){
+        rb.velocity = new Vector2(rb.velocity.x, JumpForce * JumpMultiplier);
     }
 }
--- PixelGamejam/Assets/Code/Movement/MovementStates/SpineState.cs
+++ PixelGamejam/Assets/Code/Movement/MovementStates/SpineState.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="FullState")]
-public class FullBodyScriptable : ScriptableMovementState
+[CreateAssetMenu(fileName ="SpineState")]
+public class SpineState : ScriptableMovementState
 {
     public override void Jump(){
-        rb.velocity = new Vector2(rb.velocity.x, JumpForce * JumpMultiplier);
+        
     }
 
     public override void Move(float speed)
@@ -14,4 +14,5 @@ public class FullBodyScriptable : ScriptableMovementState
         float horizontalInput = Input.GetAxis("Horizontal"); //Important!
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y); 
     }
+    
 }
